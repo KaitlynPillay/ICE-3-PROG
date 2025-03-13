c6d7d4e4f3a037a36aa1db46d8dbc33608878bce
Kaitlyn Pillay <ST10437630@vcconnect.edu.za>
2025-03-13 18:47:33 +0200

    Add project files.

---
+++ ICE 3 PROG/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ICE_3_PROG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
